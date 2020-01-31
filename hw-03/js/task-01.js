ff97ec3b5922b5ded17366bcb0591378c790371c
nNg9999 <n_n_g_@ukr.net>
2020-01-31 11:00:36 +0200

    init commit hw03

---
+++ hw-03/js/task-01.js
@@ -0,0 +1,20 @@
+'use strict';
+
+console.log('Задание 1');
+/*
+Задание 1
+Напиши скрипт, который, для объекта user, последовательно:
+
+добавляет поле mood со значением 'happy'
+заменяет значение hobby на 'skydiving'
+заменяет значение premium на false
+выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
+*/
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+console.log('Задание 1 END');
